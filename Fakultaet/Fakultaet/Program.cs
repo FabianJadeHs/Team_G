7d6189ba31456097cb48947a0bfa8b52b59524c6
FabianJadeHs <80035556+FabianJadeHs@users.noreply.github.com>
2021-03-17 10:32:13 +0100

    Fakultaet
    Neuer Code zur Falkutaetsberechnung

---
+++ Fakultaet/Fakultaet/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Fakultät_von_n_berechnen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Fenster Titel
+            Console.Title = "Fakultät von n berechnen";
+
+            // Eingabe von n
+            Console.Write("Fakultät von n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            // Fakultät berechnen
+            long result = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                result *= i;
+            }
+
+            // Ergebnis ausgeben
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
