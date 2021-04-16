7c04a07ad194ac6b344e3ab76fb05bbd3eb726fb
FabianJadeHs <80035556+FabianJadeHs@users.noreply.github.com>
2021-04-16 21:39:42 +0200

    Update Versuch.cs

--- Versuch.cs
+++ Versuch.cs
@@ -10,6 +10,34 @@ namespace Versuche
     {
         static void Main(string[] args)
         {
+    
+              {
+                double Schaftlaenge, Gewindelaenge, Schraubenlaengee;
+
+                Console.WriteLine("Wie lang soll der Schaft sein?");
+                Schaftlaenge = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Wie lang soll das Gewinde sein?");
+                Gewindelaenge = double.Parse(Console.ReadLine());
+
+                Schraubenlaengee = Schraubenlaenge(Schaftlaenge, Gewindelaenge);
+
+                Console.WriteLine(Schraubenlaengee);
+                Console.ReadKey();
+
+            }
         }
+
+        static double Schraubenlaenge(double Schaftlaenge, double Gewindelaenge) //Unterprogramm Schraubenlaenge, Eingabe Schaftlaenge/Gewindelaenge, Ausgabe Schraubenlaenge
+        {
+            double Schraubenlaengee;
+
+            Schraubenlaengee = Schaftlaenge + Gewindelaenge;
+
+            return Schraubenlaengee;
+        }
+    }
+
+}
     }
 }
