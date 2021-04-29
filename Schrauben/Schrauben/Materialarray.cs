75a64726aa1c0a0d3b776319f5c0bf331e0f0fcd
FabianJadeHs <80035556+FabianJadeHs@users.noreply.github.com>
2021-04-29 17:23:32 +0200

    Create Materialarray.cs

---
+++ Schrauben/Schrauben/Materialarray.cs
@@ -0,0 +1,16 @@
+﻿namespace Schrauben
+{
+    class Materialarray
+    {
+        //Eigenschaften des Arrays werden definiert
+        public string Materialbezeichnung { get; set; }
+        public double Materialpreis { get; set; }
+        public double Materialdichte { get; set; }
+
+        // bei Ausgabe werden die Spalten getrennt
+        public override string ToString()
+        {
+            return Materialbezeichnung + "|" + Materialpreis + "|" + Materialdichte;
+        }
+    }
+}
