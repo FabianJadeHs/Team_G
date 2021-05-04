97419ad43c5a81674101ebb34bcaee82b5a4b53e
JulianRe24 <julian.reimann@student.jade-hs.de>
2021-05-04 14:21:44 +0200

    Update Program.cs

--- Schrauben/Schrauben/Program.cs
+++ Schrauben/Schrauben/Program.cs
@@ -12,7 +12,8 @@ namespace Schrauben
         [STAThread]
         static void Main()
         {
-                        
+            new GUI_control();
+
             Schraube test1 = new Schraube();
 
             //Kundeneingaben wie er die Schraube haben möchte
@@ -44,7 +45,7 @@ namespace Schrauben
             test1.Standardausgaben();
             Console.ReadKey();
 
-            new GUI_control();
+            
             
         }
         
