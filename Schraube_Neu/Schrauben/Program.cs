f2bd1c6e4820af53d4413068d467b2add86a1e64
JulianRe24 <julian.reimann@student.jade-hs.de>
2021-05-10 21:17:01 +0200

    Update Program.cs

--- Schraube_Neu/Schrauben/Program.cs
+++ Schraube_Neu/Schrauben/Program.cs
@@ -55,6 +55,7 @@ namespace Schrauben
             test1.Flaechentraegheitsmoment();
             test1.Vorspannkraft();
             test1.Standardausgaben();
+            test1.Schluesselweite();
 
             Console.ReadKey();
 
