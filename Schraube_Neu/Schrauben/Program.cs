e1cbb361a2f745a7a82f4bd971cb1c5e72dc8b8b
arne-schlemermeyer <arne.schlemermeyer@student.jade-hs.de>
2021-05-20 17:38:19 +0200

    Update Program.cs

--- Schraube_Neu/Schrauben/Program.cs
+++ Schraube_Neu/Schrauben/Program.cs
@@ -6,19 +6,19 @@ using System.Globalization;
 
 namespace Schrauben
 {
-
+    /*
     class Program
     {
-
+        
         [STAThread]
         static void Main()
         {
             
             new GUI_control();
-                       
+
         }
         
     }
-
+    */
 }
 
