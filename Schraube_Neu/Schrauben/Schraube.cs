45a1caf0e3f4607b83cd7fb4c69703b5452a8c5d
arne-schlemermeyer <arne.schlemermeyer@student.jade-hs.de>
2021-05-25 17:57:42 +0200

    Update Schraube.cs

--- Schraube_Neu/Schrauben/Schraube.cs
+++ Schraube_Neu/Schrauben/Schraube.cs
@@ -18,13 +18,6 @@ namespace Schrauben
         public string Wunschfestigkeit { get; set; }
 
 
-        public double laenge { get; private set; }
-        public double gewindeLaenge { get; private set; }
-        public string gewindeart { get; private set; }
-        public string gewinde { get; private set; }
-        public double k { get; private set; }
-        public double Ri { get; private set; }
-        public double P { get; private set; }
 
         // globale Variablen innerhalb der class werden definiert damit Unterprogramme kürzer sind
         double rundung = 0;
@@ -286,17 +279,6 @@ namespace Schrauben
         }
         #endregion
 
-        public Schraube(double laenge, double gewindeLaenge, string gewindeart,
-           string gewinde, double k, double Ri, double P)
-        {
-            this.laenge = laenge;
-            this.gewindeLaenge = gewindeLaenge;
-            this.gewindeart = gewindeart;
-            this.gewinde = gewinde;
-            this.k = k;
-            this.Ri = Ri;
-            this.P = P;
-        }
 
     }
 }
