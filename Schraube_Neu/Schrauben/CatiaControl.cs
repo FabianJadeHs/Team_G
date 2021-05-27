78116105c64c654b2b2189da236a187541849912
be2467 <berthold.nguemewedjoumessi@student.jade-hs.de>
2021-05-27 06:43:21 +0200

    Create CatiaControl.cs

---
+++ Schraube_Neu/Schrauben/CatiaControl.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Windows;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schrauben
+{
+
+    class CatiaControl
+    {
+        CatiaControl()
+        {
+            try
+            {
+
+                CatiaConnection cc = new CatiaConnection();
+
+                // Finde Catia Prozess
+                if (cc.CATIALaeuft())
+                {
+                    Console.WriteLine("0");
+
+                    // Öffne ein neues Part
+                    cc.ErzeugePart();
+                    Console.WriteLine("1");
+
+                    // Erstelle eine Skizze
+                    cc.ErstelleLeereSkizze();
+                    Console.WriteLine("2");
+
+                    Produkt dieSchraube = new Produkt(60, 50, "Standartgewinde", "M8", 5.3d, 4d, 1.25d);
+
+                    cc.ErzeugeZylinder(dieSchraube);
+                    Console.WriteLine("Schaft");
+
+                    // cc.ErzeugeGewindeFeature();
+                    cc.ErzeugeGewindeHelix(dieSchraube);
+                    Console.WriteLine("Gewinde");
+                }
+                else
+                {
+                    Console.WriteLine("Laufende Catia Application nicht gefunden");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Exception aufgetreten");
+            }
+            Console.WriteLine("Fertig - Taste drücken.");
+            Console.ReadKey();
+
+        }
+
+        static void Main(string[] args)
+        {
+            new CatiaControl();
+        }
+    }
+
+}
+
